b4fbd4a99f3fedff757c74ba0a9fd52209d95aef
Kevin <kevin@me>
2026-07-14 20:16:17 -0100

    Added final version. Probably..

--- basic_types.hs
+++ basic_types.hs
@@ -10,9 +10,20 @@ nope False = True
 -- Lets create a simple program:
 -- Define my own type, which has values of: yes, nar. Which has opposite values.
 -- Then define a morphism which takes in in a yes or nar and morphs into nar or yes
+data MyType where
+    Yes :: MyType
+    Nar :: MyType
+    deriving (Show) -- So we can print
+
+thinkNot :: MyType -> MyType -- Type Signature
+thinkNot Yes = Nar -- Equations. Must the inline with the Type Signature due to scoping. But feels kinda gross. \
+thinkNot Nar = Yes -- It's feels gross, because a Type Signature (should?) be coupled with the equations. \
+                   -- Though, it's kinda ok, if you think Type Signature and Equations are indepentent of each other.
 
 main :: IO ()
 main = do
     print (nope yes)
+    print (thinkNot yay)
         where
+            yay = Yes
             yes = True
