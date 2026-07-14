e3276b5dd2cbf4adb13507857ed451bc5e5d6513
Kevin <kevin@me>
2026-07-14 19:24:26 -0100

    Added so it can actually compile

--- basic_types.hs
+++ basic_types.hs
@@ -2,15 +2,17 @@
 -- Type Error
 -- Decleration vs Equations
 
+-- Step 1. Lets create our own 'not' "operator" and call it nope
+nope :: Bool -> Bool -- Type Signature
+nope True = False
+nope False = True
+
 -- Lets create a simple program:
 -- Define my own type, which has values of: yes, nar. Which has opposite values.
 -- Then define a morphism which takes in in a yes or nar and morphs into nar or yes
 
-nope :: Bool -> Bool -- Type Signature: nope has value in type Bool -> Bool, which is a morphism
-nope x = !x -- Decleration (nope x =)
-
-
 main :: IO ()
 main = do
-    let yes = True
     print (nope yes)
+        where
+            yes = True
